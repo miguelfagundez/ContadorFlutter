83750e338e68148a7274c995002da5e08eaf1b7a
Miguel <fagundezdev@gmail.com>
2021-01-28 14:34:03 -0500

    Add floating btn

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,17 +1,38 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget{
+
+  // Text
+  final bodyText = 'Number of clicks: ';
+  final appText = 'Contador App';
+
+  //Styles
+  final estilo = TextStyle( fontSize: 25.0);
 
   @override
   Widget build(BuildContext context   ) {
       return Scaffold(
         appBar: AppBar(
-          title: Text('Titulo Home Page'),
+          title: Text(appText),
           centerTitle: true,
           elevation: 0.0,
         ),
         body: Center(
-          child: Text('Home Page Body'),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Text(bodyText, style: estilo,),
+              Text('0', style: estilo,),
+            ],
+          ),
+        ),
+        floatingActionButtonLocation: FloatingActionButtonLocation.miniStartFloat,
+        floatingActionButton: FloatingActionButton(
+          onPressed: (){
+            print('Pressed');
+          },
+          child: Icon(Icons.plus_one),
         ),
       );
   }
